edfdd0e40fcbe359c562d473ab738db19ff25d61
bonatti123 <anthony.bonatti.j@gmail.com>
2026-03-19 08:52:16 -0500

    :bug:corregir

--- src/components/skeletons/PokemonSkeleton.jsx
+++ src/components/skeletons/PokemonSkeleton.jsx
@@ -2,7 +2,6 @@ function PokemonSkeleton() {
   return (
     <div className="relative overflow-hidden bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md">
 
-      {/* EFECTO SHIMMER */}
       <div className="absolute inset-0 -translate-x-full animate-[shimmer_1.5s_infinite] bg-gradient-to-r from-transparent via-white/40 to-transparent"></div>
 
       <div className="w-24 h-24 mx-auto rounded-full bg-gray-300 dark:bg-gray-600"></div>
