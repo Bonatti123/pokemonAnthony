ca28498dac3d0bd08ebb91663351fb18f32f9a67
bonatti123 <anthony.bonatti.j@gmail.com>
2026-03-19 09:02:12 -0500

    :fire:comentarios

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./core/App";
 import "./styles/global.css";
 
-// 🔥 IMPORTANTE
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // crear cliente
@@ -11,7 +10,6 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* ENVOLVER TODA LA APP */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
