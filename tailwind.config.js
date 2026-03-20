8b07b409a74e0c785b1c337a3f1ca34316faeb16
bonatti123 <anthony.bonatti.j@gmail.com>
2026-03-20 10:49:50 -0500

    feat ":white_check_mark: Configura Tailwind CSS, Activa el modo oscuro (darkmode: "class"), Define dónde Tailwind busca clases en el proyecto"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { animate, keyframes } from "framer-motion";
+
 export default {
   darkMode: "class",
   content: [
@@ -8,4 +10,20 @@ export default {
     extend: {},
   },
   plugins: [],
+
+
+theme: {
+  extend: {
+    keyframes: {
+      shimmer: {
+        '100%': { transform: 'translateX(100%)' },
+      },
+    },
+    // Aqui podriamos definir una animación personalidad con otro tiempo
+    animation: {
+      shimmer: 'shimmer 1.5s infinite',
+    },
+  },
+}
+
 };
